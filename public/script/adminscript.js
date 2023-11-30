84277fab5aa1d03f6885124a8cfc663ed1d1e4c5
Mithun <mrmithunofficial321@gmail.com>
2023-12-01 00:30:06 +0530

    Implimented admin side user search feature

--- public/script/adminscript.js
+++ public/script/adminscript.js
@@ -61,26 +61,45 @@ if(productDltButtons){
 if(userSearchForm){
   userSearchForm.addEventListener('submit', async function(e){
    e.preventDefault();
-   for(let i=0; i<usernavs.length; i++){
-       console.log(usernavs[i]);
-       usernavs[i].remove();
+   const usernavsArray = Array.from(usernavs);
+   for(let i=0; i<usernavsArray.length; i++){
+       usernavsArray[i].remove();
    }
-  //  const inputValue = userNameInput.value;
-  //  const res = fetch('/admin/panel/user_search',{
-  //    method : 'POST',
-  //    headers : {
-  //     "Content-type" : "application/json"
-  //    },
-  //    body : JSON.stringify({
-  //      searchValue : inputValue
-  //    })
-  //  })
-  //  res.then((response)=>{
-  //  return response.json();
-  //  })
-  //  .then((data)=>{
-
-  //   console.log(data);
-  //  })
+   const inputValue = userNameInput.value;
+   const res = fetch('/admin/panel/user_search',{
+     method : 'POST',
+     headers : {
+      "Content-type" : "application/json"
+     },
+     body : JSON.stringify({
+       searchValue : inputValue
+     })
+   })
+   res.then((response)=>{
+   return response.json();
+   })
+   .then((data)=>{
+    const userSearchContainer = document.querySelector('.user-searchContainer');
+    data.result.forEach((data)=>{
+      const result = document.createElement('div');
+      result.innerHTML = `
+      <nav class="navbar mt-1 user-nav rounded">
+      <div class="container">
+        <div class="">
+          <img class="img-thumbnail" src="https://cdn-icons-png.flaticon.com/512/5511/5511365.png" alt="Bootstrap" width="30" height="24">
+          <span class="user_name"> ${data.username} </span>
+        </div>
+        <div>
+          <p class="pe-5 user_email"> ${data.email} </p>
+        </div>
+        <div>
+          <a href="/admin/panel/user_management/delete_user/${data._id}"><button type="button" class="btn btn-danger user_delete_btn">Delete</button></a>
+          <a href="/admin/panel/user_management/edit_user/${data.id}"><button type="button" class="btn btn-primary user_edit_btn">Edit</button></a>
+        </div>
+      </div>
+    </nav>`
+    userSearchContainer.appendChild(result);
+    })
+   })
   })
 }
